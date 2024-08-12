459c964c5272a740cc51775c9e4fedabdd6908b1
Seberestmain <berest.alescha@gmail.com>
2024-08-12 15:36:49 +0300

    Создан класс HotDrinks

---
+++ src/HotDrinks.java
@@ -0,0 +1,25 @@
+public class HotDrinks {
+    private String name;
+    private int volume;
+
+    public HotDrinks(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrinks{" +
+                "name='" + name + '\'' +
+                ", volume=" + volume +
+                '}';
+    }
+}
