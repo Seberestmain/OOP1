56179d39585d76fa087aa27e29ac0bd7a1b9ec66
Seberestmain <berest.alescha@gmail.com>
2024-08-12 15:43:26 +0300

    Метод main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        HotDrinkWithTemperature coffee = new HotDrinkWithTemperature("Кофе", 200, 80);
+        HotDrinkWithTemperature tea = new HotDrinkWithTemperature("Чай", 250, 75);
+        HotDrinkWithTemperature cocoa = new HotDrinkWithTemperature("Какао", 300, 85);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        VendingMachineHotDrinks vendingMachine = new VendingMachineHotDrinks();
+        vendingMachine.addDrink(coffee);
+        vendingMachine.addDrink(tea);
+        vendingMachine.addDrink(cocoa);
+
+
+        HotDrinks selectedDrink = vendingMachine.getProduct("Чай", 250, 75);
+
+        if (selectedDrink != null) {
+            System.out.println("Выбран" + selectedDrink);
+        } else {
+            System.out.println("Такого напитка нет");
         }
     }
 }
